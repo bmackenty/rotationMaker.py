0efd2c7616675edf6b032f3eecfb35e54327a22d
Bill MacKenty <bmackenty@users.noreply.github.com>
2019-08-15 10:14:03 +0200

    updated for 2019-2020 school year.
    This isn't ready for production yet :-)

--- 11and12rotationmaker.py
+++ 11and12rotationmaker.py
@@ -1,55 +1,40 @@
 #
-# This program is designed to make a rotating calendar for the American School of Warsaw. 
-# 
+# This program is designed to make a  rotating calendar for the American School of Warsaw. 
+# The program accounts for vacations, weekends and wednesdays, which have a different meeting time.
+# This creates a rotating calendar for student contact days only. This creates output designed to be imported into a google calendar.
+#
+# A command line scenario might be $11_12rotation.py > 11_12_rotaion.txt
+# The txt file would then be imported into google calendar. 
+#
+# I used this calendar as authoritative reference: 
+# https://resources.finalsite.net/images/v1539954537/warsaw/kmdb4gksa4mu7rjfwhcj/Calendar2019-2020Final.pdf
+#
+# Questions, comments to Bill MacKenty bmackenty@gmail.com
+# github repo: https://github.com/bmackenty/rotationMaker.py
+
+# import various libraries:
+
 import datetime
+import os
 from dateutil.rrule import rrule, DAILY
-
-# initialization stuff for 9th and 10th grade! 
-
-grade9and10normalPeriod1Start = datetime.time(8,30)
-grade9and10normalPeriod1End = datetime.time(9,40)
-
-grade9and10normalPeriod2Start = datetime.time(10,00)
-grade9and10normalPeriod2End = datetime.time(11,10)
-
-grade9and10normalPeriod3Start = datetime.time(11,20)
-grade9and10normalPeriod3End = datetime.time(12,30)
-
-grade9and10normalPeriod4Start = datetime.time(14,20)
-grade9and10normalPeriod4End = datetime.time(15,30)
-
-# the configuration below is for Wednesdays
-
-grade9and10WednesdayPeriod1Start = datetime.time(9,30)
-grade9and10WednesdayPeriod1End = datetime.time(10,45)
-
-grade9and10WednesdayPeriod2Start = datetime.time(11,00)
-grade9and10WednesdayPeriod2End = datetime.time(12,15)
-
-grade9and10WednesdayPeriod3Start = datetime.time(12,55)
-grade9and10WednesdayPeriod3End = datetime.time(14,10)
-
-grade9and10WednesdayPeriod4Start = datetime.time(14,15)
-grade9and10WednesdayPeriod4End = datetime.time(15,30)
-
 
 
 # initialize the grade 11 and 12 
 
 
 grade11and12normalPeriod1Start = datetime.time(8,20)
-grade11and12normalPeriod1End = datetime.time(9,45)
+grade11and12normalPeriod1End = datetime.time(9,40)
 
-grade11and12normalPeriod2Start = datetime.time(9,50)
-grade11and12normalPeriod2End = datetime.time(11,15)
+grade11and12normalPeriod2Start = datetime.time(10,00)
+grade11and12normalPeriod2End = datetime.time(11,20)
 
 grade11and12normalPeriod3Start = datetime.time(11,30)
-grade11and12normalPeriod3End = datetime.time(12,55)
+grade11and12normalPeriod3End = datetime.time(12,50)
 
-grade11and12normalPeriodADVStart = datetime.time(13,00)
-grade11and12normalPeriodADVEnd = datetime.time(13,20)
+grade11and12normalPeriodADVStart = datetime.time(12,55)
+grade11and12normalPeriodADVEnd = datetime.time(13,15)
 
-grade11and12normalPeriod4Start = datetime.time(13,55)
+grade11and12normalPeriod4Start = datetime.time(14,00)
 grade11and12normalPeriod4End = datetime.time(15,20)
 
 # the configuration below is for Wednesdays
@@ -67,24 +52,59 @@ grade11and12WednesdayPeriod4Start = datetime.time(14,15)
 grade11and12WednesdayPeriod4End = datetime.time(15,30)
 
 
-# Initialize vacations 
-
-vacations = ["2018-09-06","2018-09-07","2018-10-05","2018-10-17","2018-10-29","2018-10-30","2018-10-31","2018-11-01","2018-11-02",
-"2018-11-22","2018-11-23",
-"2018-12-17","2018-12-18","2018-12-19","2018-12-20","2018-12-21","2018-12-24","2018-12-25","2018-12-26","2018-12-27","2018-12-28","2018-12-31","2019-01-01","2019-01-02","2019-01-03","2019-01-04",
-"2019-02-12","2019-02-18","2019-02-19","2019-02-20","2019-02-21","2019-02-22",
-"2019-03-15","2019-04-19","2019-04-22","2019-04-29","2019-04-30","2019-05-01","2019-05-02","2019-05-03",
-"2019-06-19"]
-
-
 
 # initialization for start of year
 
-startOfAcademicYear = datetime.date(2018,8,24) # Friday
-endOfAcademicYear = datetime.date(2019,6,18) # Wednesday
+startOfAcademicYear = datetime.date(2019,8,23) # Friday
+endOfAcademicYear = datetime.date(2020,6,18) # Thursday
 academicYearMeetingDays = endOfAcademicYear - startOfAcademicYear
 
-# functions to do stuff
+# print("There are", academicYearMeetingDays.days, "total calendar days, including weekends and vacations. ")
+
+# Initialize vacations and staff PD days.  Please triple-check this!!!!
+
+noStudentContactDays = [
+    "2019-10-04",
+    "2019-10-28",
+    "2019-10-29",
+    "2019-10-30",
+    "2019-10-31",
+    "2019-11-01",
+    "2019-11-11",
+    "2019-11-28",
+    "2019-11-29",
+    "2019-12-16",
+    "2019-12-17",
+    "2019-12-18",
+    "2019-12-19",
+    "2019-12-20",
+    "2019-12-23",
+    "2019-12-24",
+    "2019-12-25",
+    "2019-12-26",
+    "2019-12-27",
+    "2019-12-30",
+    "2019-12-31",
+    "2020-01-01",
+    "2020-01-02",
+    "2020-01-03",
+    "2020-01-06",
+    "2020-02-24",
+    "2020-02-25",
+    "2020-02-26",
+    "2020-02-27",
+    "2020-02-28",
+    "2020-03-02",
+    "2020-04-10",
+    "2020-04-13",
+    "2020-04-27",
+    "2020-04-28",
+    "2020-04-29",
+    "2020-04-30",
+    "2020-05-01",
+    "2020-06-11"
+    ]
+# function to translate interger weekday to human-readable date
 
 def dayOfWeek(integerWeekday):
 	if integerWeekday == 0:
@@ -132,9 +152,8 @@ meetingDayNumber = 1
 
 for dt in rrule(DAILY, dtstart=startOfAcademicYear, until=endOfAcademicYear):
 		# the condition below skips weekends
-		# TODO add 9/10/11/12 split 
 		if dt.weekday() < 5:
-			if dt.strftime('%Y-%m-%d') not in vacations:
+			if dt.strftime('%Y-%m-%d') not in noStudentContactDays:
 				# the condition below limits us to 4 rotating days
 				if meetingDayNumber > 4:
 					meetingDayNumber = 1
