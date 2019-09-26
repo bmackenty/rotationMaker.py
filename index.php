66ab5672c99898a8a2bccc1279fe2124ea964208
Bill MacKenty <bmackenty@users.noreply.github.com>
2019-09-26 11:58:05 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,63 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    
+    <!-- =========================================== -->
+    <!-- PLEASE DO NOT EDIT ANYTHING ABOVE THIS LINE -->
+    <!-- =========================================== -->
+
+<div class="border border-secondary p-3 m-4">
+
+<div class="alert alert-warning alert-dismissible fade show" role="alert">
+  <strong>Warning:</strong> This web application is in active development. Don't use it until this message changes. 23 September 2019 
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+
+<div class="card mb-3">
+  <div class="card-header">
+    Welcome
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">Rotation maker</h5>
+    <p class="card-text">This application helps to create rotating calendars for google calendar for the school year. This application only works for ASW. </p>
+    <p class="card-text">Please select the correct scheduling scenario below.  Each schedule is a bit different, so its important you select the correct scenario.</p>
+    <p class="card-text">If you don't see a scenerio which fits your specific situation, please <a href="contact.php">contact us</a>.
+  </div>
+</div>
+
+
+
+<div class="row m-1">
+  <ul class="list-group">
+    <li class="list-group-item"><a class="btn btn-primary" href="self_service_elementary.php" role="button">Click here if you <strong>only</strong> teach in the elementary school (grades k to 5).</a></li>
+    <li class="list-group-item"><a class="btn btn-primary" href="self_service_middle.php" role="button">Click here if you <strong>only</strong> teach in the middle school (grades 6 to 8).</a></li>
+    <li class="list-group-item"><a class="btn btn-primary" href="self_service_high.php" role="button">Click here if you teach in the high school and teach <strong>NO</strong> DP courses.</a></li>
+    <li class="list-group-item"><a class="btn btn-primary" href="self_service_high.php" role="button">Click here if you teach in the high school and teach <strong>ANY</strong> DP courses.</a></li>
+    <li class="list-group-item"><a class="btn btn-primary" href="self_service_mixed.php" role="button">Click here if you teach in a mix of schools</a></li>
+  </ul>
+</div>
+
+
+
+
+
+</div> <!-- this closes the container div -->
+    <!-- =========================================== -->
+    <!-- PLEASE DO NOT EDIT ANYTHING BELOW THIS LINE -->
+    <!-- =========================================== -->
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
